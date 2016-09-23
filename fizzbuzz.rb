1dc2b4937fce66dfdd486b81296a85cd30cf2b75
alexanderwjrussell <awjrussell@live.co.uk>
2016-09-23 16:04:11 +0100

    Code fully working and refactored

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,10 @@
 class FizzBuzz
 
   def number(num)
-    if num % 15 == 0
-      return "FizzBuzz"
-    elsif num % 3 == 0
-      return "Fizz"
-    elsif num % 5 == 0
-      return "Buzz"
-    else
-      return num
-    end
+    return "FizzBuzz" if num % 15 == 0
+    return "Fizz" if num % 3 == 0
+    return "Buzz" if num % 5 == 0
+    num
   end
-
+  
 end
