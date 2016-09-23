d97513f67f1d617fe128b5aaa213d4ee7faf45f0
alexanderwjrussell <awjrussell@live.co.uk>
2016-09-23 15:04:19 +0100

    Second test added and failing

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,8 +6,12 @@ describe FizzBuzz do
     @FizzBuzz = FizzBuzz.new
   end
 
-  it "Return fizz when passed a 3" do
-    expect(@FizzBuzz.number(3)). to eq("Fizz")
+  it "Return Fizz when passed a 3" do
+    expect(@FizzBuzz.number(3)).to eq("Fizz")
+  end
+
+  it "Return Buzz when passed a 5" do
+    expect(@FizzBuzz.number(5)).to eq("Buzz")
   end
 
 end
